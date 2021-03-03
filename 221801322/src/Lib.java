cf041d5059db8287298f7fa0bb384133ae487d20
caihan <2249618917@qq.com>
2021-03-03 18:20:43 +0800

    commit 根据编辑器提示复审代码

--- 221801322/src/Lib.java
+++ 221801322/src/Lib.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,16 +16,16 @@ public class Lib {
      * @description 计算核心模块，只负责计算和返回计算结果
      */
     static class CoreModule {
-        private File inputFile;
-        private Map<String,Integer> wordFreq;
+        private final File INPUT_FILE;
+        private final Map<String,Integer> WORD_FREQ;
         private boolean isCountWordFreq;
         /**
          * @description CoreModule类构造函数
          * @param inputPath
          */
         public CoreModule(String inputPath) {
-            inputFile = new File(inputPath);
-            wordFreq = new HashMap<>();
+            INPUT_FILE = new File(inputPath);
+            WORD_FREQ = new HashMap<>();
             isCountWordFreq = false;
         }
         /**
@@ -32,11 +33,11 @@ public class Lib {
          * @return charNum
          */
         public int countChar() throws IOException {
-            FileReader fileReader = new FileReader(inputFile);
+            FileReader fileReader = new FileReader(INPUT_FILE);
             int fileChar;
             int charNum = 0;
             while((fileChar = fileReader.read()) != -1) {
-                if ((0 <= fileChar) && (127 >= fileChar)) {
+                if (fileChar <= 127) {
                     charNum++;
                 }
             }
@@ -52,7 +53,7 @@ public class Lib {
                 isCountWordFreq = true;
             }
             int wordNum = 0;
-            for(int value : wordFreq.values()) {
+            for(int value : WORD_FREQ.values()) {
                 wordNum += value;
             }
             return wordNum;
@@ -62,8 +63,8 @@ public class Lib {
          * @return lineNum
          */
         public int countLine() throws IOException {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
-            String lineStr = "";
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(INPUT_FILE));
+            String lineStr;
             int lineNum = 0;
             while ((lineStr = bufferedReader.readLine()) != null) {
                 if(NON_BLACK_PATTERN.matcher(lineStr).matches()) {
@@ -81,10 +82,10 @@ public class Lib {
                 extractWord();
                 isCountWordFreq = true;
             }
-            ArrayList<Map.Entry<String,Integer>> wordList = new ArrayList<>(wordFreq.entrySet());
+            ArrayList<Map.Entry<String,Integer>> wordList = new ArrayList<>(WORD_FREQ.entrySet());
             LinkedHashMap<String,Integer> mostFreqWord = new LinkedHashMap<>();
-            Collections.sort(wordList,(map1,map2) -> {
-                if(map1.getValue() == map2.getValue()) {
+            wordList.sort((map1, map2) -> {
+                if (map1.getValue().equals(map2.getValue())) {
                     return map1.getKey().compareTo(map2.getKey());
                 } else {
                     return map2.getValue().compareTo(map1.getValue());
@@ -105,17 +106,18 @@ public class Lib {
          * @return void
          */
         private void extractWord() throws IOException {
-            FileInputStream fileInputStream = new FileInputStream(inputFile);
+            FileInputStream fileInputStream = new FileInputStream(INPUT_FILE);
             int strSize = fileInputStream.available();
             byte[] strBuffer = new byte[strSize];
+            //noinspection ResultOfMethodCallIgnored
             fileInputStream.read(strBuffer);
-            String fileStr = new String(strBuffer,"UTF-8");
+            String fileStr = new String(strBuffer,StandardCharsets.UTF_8);
             Matcher matcher = WORD_PATTERN.matcher(fileStr);
             while(matcher.find()) {
-                if(wordFreq.containsKey(matcher.group(0))) {
-                    wordFreq.put(matcher.group(0),(wordFreq.get(matcher.group(0)) + 1));
+                if(WORD_FREQ.containsKey(matcher.group(0))) {
+                    WORD_FREQ.put(matcher.group(0),(WORD_FREQ.get(matcher.group(0)) + 1));
                 } else {
-                    wordFreq.put(matcher.group(0),1);
+                    WORD_FREQ.put(matcher.group(0),1);
                 }
             }
         }
@@ -124,27 +126,25 @@ public class Lib {
      * @description 回答问题模块，负责调用计算核心进行统计并将结果写入文件
      */
     static class AnswerModule {
-        private File outputFile;
-        private BufferedWriter bufferedWriter;
-        private int charNum;
-        private int lineNum;
-        private int wordNum;
-        private LinkedHashMap<String,Integer> mostFreqWord;
+        private final int CHAR_NUM;
+        private final int WORD_NUM;
+        private final int LINE_NUM;
+        private final LinkedHashMap<String,Integer> MOST_FREQ_WORD;
         public AnswerModule(CoreModule core) throws IOException {
-            charNum = core.countChar();
-            lineNum = core.countWord();
-            wordNum = core.countLine();
-            mostFreqWord = core.countWordFreq();
+            CHAR_NUM = core.countChar();
+            WORD_NUM = core.countWord();
+            LINE_NUM = core.countLine();
+            MOST_FREQ_WORD = core.countWordFreq();
         }
         public void writeToFile(String outputPath) throws IOException {
-            outputFile = new File(outputPath);
-            bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+            File outputFile = new File(outputPath);
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
             StringBuilder resultStr = new StringBuilder();
-            resultStr.append("characters: " + charNum + "\n");
-            resultStr.append("words: " + wordNum + "\n");
-            resultStr.append("lines: " + lineNum + "\n");
-            for(Map.Entry<String,Integer> entry: mostFreqWord.entrySet()) {
-                resultStr.append(entry.getKey() + ": " + entry.getValue() + "\n");
+            resultStr.append("characters: ").append(CHAR_NUM).append("\n");
+            resultStr.append("words: ").append(WORD_NUM).append("\n");
+            resultStr.append("lines: ").append(LINE_NUM).append("\n");
+            for(Map.Entry<String,Integer> entry : MOST_FREQ_WORD.entrySet()) {
+                resultStr.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
             }
             bufferedWriter.write(resultStr.toString());
             bufferedWriter.close();
